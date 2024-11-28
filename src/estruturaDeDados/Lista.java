bbd948e1dced6b9e917e346ae472229201211d3e
mariiyagraziela <90150122+mariiyagraziela@users.noreply.github.com>
2024-11-28 09:49:27 -0300

    Update Lista.java

--- src/estruturaDeDados/Lista.java
+++ src/estruturaDeDados/Lista.java
@@ -1,20 +1,41 @@
 package estruturaDeDados;
 
+import java.util.LinkedList;
+
+import java.util.LinkedList;
+
 public class Lista {
 
     public Lista() {
         // TODO Auto-generated constructor stub
         throw new UnsupportedOperationException("Unimplemented constructor 'ListaCircular'");
+        LinkedList<int> lista = new LinkedList<>();
     }
 
-    public void insereInicio(int i) {
+    public static insereInicio(int[] array, int elemento) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'insereInicio'");
+        int[] temArray = new int[array.length[]];
+        for(int i = 0 ; i<array.length ; i++){
+            tempArray[i] = array[i]; 
+        } 
+        tempArray[array.length + 1 ] = elemento;
+        return temArray;
     }
 
-    public boolean buscaElemento(int i) {
+    public boolean buscaElemento(int[] array, int elemento) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'buscaElemento'");
+        for(int i = 0 ; i<array.length ; i++){
+            boolean elementoExiste = false;
+            tempArray[i] = array[i]; 
+            if (tempArray[i] == elemento){
+                elementoExiste = true;
+                int indiceDoElemento = i ;
+            }
+        return elementoExiste ;    
+        } 
+    
     }
 
     public Object buscaIndice(int i) {
